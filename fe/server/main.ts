b79d380ee45a509af08c500071ebb1a80e7e49c9
Sean T Canavan <seantcanavan@gmail.com>
2023-08-06 10:06:15 -0500

    almost done with deno server example
    Signed-off-by: Sean T Canavan <seantcanavan@gmail.com>

---
+++ fe/server/main.ts
@@ -0,0 +1,31 @@
+// @deno-types="npm:@types/express@4"
+import express, { NextFunction, Request, Response } from "npm:express@4.18.2";
+import demoData from "./data_blob.json" assert { type: "json" };
+
+const app = express();
+const port = Number(Deno.env.get("PORT")) || 3000;
+
+const reqLogger = function (req, _res, next) {
+    console.info(`${req.method} request to "${req.url}" by ${req.hostname}`);
+    next();
+};
+
+app.use(reqLogger);
+
+app.get("/users", (_req, res) => {
+    res.status(200).json(demoData.users);
+});
+
+app.get("/users/:id", (req, res) => {
+    const idx = Number(req.params.id);
+    for (const user of demoData.users) {
+        if (user.id === idx) {
+            res.status(200).json(user);
+        }
+    }
+    res.status(400).json({ msg: "User not found" });
+});
+
+app.listen(port, () => {
+    console.log(`Listening on ${port} ...`);
+});
